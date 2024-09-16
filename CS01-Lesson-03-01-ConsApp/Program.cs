57cc48e75d69e97211b77c2037ec9cd1c2d3bed3
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-16 23:30:28 +0500

    Commit практического задания №1

---
+++ CS01-Lesson-03-01-ConsApp/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1. Найти максимальное число.
+// На вход программе подаётся последовательность чисел, заканчивающаяся нулём.
+// Найти максимальное число.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_03_01_ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int max = a;
+
+            while (a != 0)
+            {
+                a = int.Parse(Console.ReadLine());
+                if (a > max) max = a;
+            }
+
+            Console.WriteLine(max);
+            Console.ReadLine();
+        }
+    }
+}
