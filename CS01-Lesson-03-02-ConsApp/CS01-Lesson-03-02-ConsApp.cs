024cabea3a2ab86f8fe658cc9ff471262e194011
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-18 13:05:38 +0500

    Commit практического задания №2

---
+++ CS01-Lesson-03-02-ConsApp/CS01-Lesson-03-02-ConsApp.cs
@@ -0,0 +1,29 @@
+﻿// Задача 2. Написать функцию обмена значениями двух переменных.
+// Передача параметров по ссылке — ref.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_03_02_ConsApp
+{
+    class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Swap(ref a, ref b);
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.ReadLine();
+        }
+    }
+}
