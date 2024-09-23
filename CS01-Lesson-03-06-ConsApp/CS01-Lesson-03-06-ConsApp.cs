6ea447a3278a5cc872da64f6b21d38b80f5db362
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-24 04:32:58 +0500

    Commit практического задания №6

---
+++ CS01-Lesson-03-06-ConsApp/CS01-Lesson-03-06-ConsApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_03_06_ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
