44aac0f9891efe105d7aa5c8115fb5b23703e525
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-26 05:45:38 +0500

    Commit практического задания №8

---
+++ CS01-Lesson-03-08-ConsApp/CS01-Lesson-03-08-ConsApp.cs
@@ -0,0 +1,34 @@
+﻿// Задача 8. Написать программу табуляции произвольной функции в диапазоне
+// от a до b.
+// Написать программу табуляции произвольной функции в диапазоне от a до b.
+// Функция задаётся программно.
+// а) Решение без использования ООП.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_03_08_ConsApp
+{
+    class Program
+    {
+        static double F(double x)
+        {
+            return 1 / x;
+        }
+
+        static void Main(string[] args)
+        {
+            double a = -5;
+            double b = 5;
+            double h = 0.5;
+
+            Console.WriteLine("{0,10} {1,10}", "x", "F(x)");
+            for (double  x = a; x <= b; x+=h)
+                Console.WriteLine("{0,10} {1,10}", x, F(x));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CS01-Lesson-03-09-ConsApp/CS01-Lesson-03-09-ConsApp.cs
@@ -0,0 +1,20 @@
+﻿//Задача 9. Написать программу табуляции произвольной функции в диапазоне
+//от a до b.
+//Написать программу табуляции произвольной функции в диапазоне от a до b.
+//Функция задаётся программно.
+//б) Решение с использованием ООП.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_03_09_ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
