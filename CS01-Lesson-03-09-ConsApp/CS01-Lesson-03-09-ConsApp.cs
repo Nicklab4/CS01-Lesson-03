e92a05be80774909cf70e4073524e0ead821c567
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-26 06:07:49 +0500

    Commit практического задания №10

--- CS01-Lesson-03-09-ConsApp/CS01-Lesson-03-09-ConsApp.cs
+++ CS01-Lesson-03-09-ConsApp/CS01-Lesson-03-09-ConsApp.cs
@@ -11,6 +11,43 @@ using System.Text;
 
 namespace CS01_Lesson_03_09_ConsApp
 {
+    class Table
+    {
+        double a = -5;
+        double b = 5;
+        double h = 0.5;
+
+        public Table()
+        {
+        }
+
+        public Table(double a, double b, double h)
+        {
+            this.a = a;
+            this.b = b;
+            this.h = h;
+        }
+
+        double F(double x)
+        {
+            return 1 / x;
+        }
+
+        public void Show(double a, double b, double h)
+        {
+            Console.WriteLine("{0.10} {1.10}", "x", "F(x)");
+            for (double x = a; x <= b; x = x + h)
+                Console.WriteLine("{0.10} {1.10}", x, F(x));
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("{0.10} {1.10}", "x", "F(x)");
+            for (double x = a; x <= b; x = x + h)
+                Console.WriteLine("{0.10} {1.10}", x, F(x));
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
---
+++ CS01-Lesson-03-10-ConsApp/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 4. Игра «Угадай число». Метод половинного деления.
+//Написать игру «Угадай число». Компьютер загадывает число в диапазоне от 1 до 100,
+//а человек за ограниченное число попыток должен угадать его. Количество попыток
+//вычисляется по формуле i=log 2 N+1
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_03_10_ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 1;
+            int max = 100;
+            int maxCount = (int)Math.Log(max - min + 1, 2) + 1;
+            int count = 0;
+
+            Random rnd = new Random();
+            int guessNumber = rnd.Next(min, max);
+
+            Console.WriteLine("Компьютер загадал число от {0} до {1}. Попробуйте его угадать за {2} попыток",
+                min, max, maxCount);
+
+            int n;
+            do
+            {
+                count++;
+                Console.WriteLine("{0} попытка. Введите число", count);
+                n = int.Parse(Console.ReadLine());
+                if (n > guessNumber) Console.WriteLine("Перелёт!");
+                if (n < guessNumber) Console.WriteLine("Недолёт!");
+            }
+            while (count < maxCount && n != guessNumber);
+
+            if (n == guessNumber) Console.WriteLine("Поздравляю! Вы угадали число за {0} попыток!", count);
+            else Console.WriteLine("Неудача! Попробуйте ещё раз.");
+
+            Console.ReadLine();
+        }
+    }
+}
