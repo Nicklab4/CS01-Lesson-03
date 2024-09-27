7d01e2638f815d126e6a5d69735ae326d25ede73
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-27 06:03:46 +0500

    Commit практического задания №11

---
+++ CS01-Lesson-03-11-ConsApp/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 11.
+//Структура Point
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_03_11_ConsApp
+{
+    class Program
+    {
+        struct Point
+        {
+            double _x, _y;
+
+            public Point(double x, double y)
+            {
+                _x = x;
+                _y = y;
+            }
+
+            public double Distance(Point z)
+            {
+                return Math.Sqrt(Math.Pow(_x - z._x, 2) + Math.Pow(_y - z._y, 2));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Point C;
+
+            Point A = new Point(0, 0);
+            Point B = new Point(2, 3);
+            Console.WriteLine(A.Distance(B));
+
+            Console.ReadLine();
+        }
+    }
+}
